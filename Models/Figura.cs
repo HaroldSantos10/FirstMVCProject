ed0c6dfb271457b48151010915daae986fcc7691
Meylin Reyes <reyesmeylin20@gmail.com>
2021-11-26 15:40:55 -0600

    Actualizacion de Meylin

--- Models/Figura.cs
+++ Models/Figura.cs
@@ -6,7 +6,20 @@ namespace ComputProject.Models
 {
     public class Figura
     {
+        [Key]
+        public int figuraId{get; set;}
 
+        [Required(ErrorMessage="Nombre es requerido")]
+        [Display(Name="Nombre")]
+        public string nombre {get; set;}
+
+        [Required(ErrorMessage="Lados es requerido")]
+        [Display(Name="Lados")]
+        public string lados {get; set;}
+
+        [Required(ErrorMessage="Angulo es requerido")]
+        [Display(Name="Angulo")]
+        public string angulo {get; set;}
     }
     
 }
