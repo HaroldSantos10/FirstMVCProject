6023c14a4f280b6baf1adc829db2782a046cc08b
¨Liss <lissramos2812@mail.com>
2021-11-26 15:19:34 -0600

    Rama Liseth

--- Models/Documento.cs
+++ Models/Documento.cs
@@ -7,6 +7,25 @@ namespace ComputProject.Models
 {
     public class Documento
     {
+         [Key]
 
+        public int DocumentoId{get; set;}
+
+        [Required(ErrorMessage="Titulo es requerido")]
+        [Display(Name="Titulo")]
+        public string titulo {get; set;}
+
+        [Required(ErrorMessage="Contenido es requerido")]
+        [Display(Name="Contenido")]
+        public string contenido {get; set;}
+
+        [Required(ErrorMessage="fecha de Publicación es requerida")]
+        [DataType(DataType.Date)]
+        [Display(Name="FechaPublicacion")]
+        public string fechapublicacion {get; set;}
+
+        [Required(ErrorMessage="Descripcion es requerida")]
+        [Display(Name="Descripcion")]
+        public string descripcion {get; set;}
     }
 }
