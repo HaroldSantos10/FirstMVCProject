ae0156b8605aba2be5071e37965d72a1aee5e289
Fatima Ayala <ftayala89@gmail.com>
2021-11-26 16:35:46 -0600

    Actualizacion

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -7,6 +7,24 @@ namespace ComputProject.Models
 {
     public class Cliente
     {
+    [Key]
 
+        public int clienteId{get; set;}
+
+        [Required(ErrorMessage="Name is requeried")]
+        [Display(Name="Names")]
+        public string names {get; set;}
+
+        [Required(ErrorMessage="Address is requeried")]
+        [Display(Name="Address")]
+        public string address {get; set;}
+
+        [Required(ErrorMessage="Telephone is requeried")]
+        [Display(Name="Telephone")]
+        public string telephone {get; set;}
+
+        [Required(ErrorMessage="Mobile is requeried")]
+        [Display(Name="Mobile")]
+        public string mobile {get; set;}
     }
 }
